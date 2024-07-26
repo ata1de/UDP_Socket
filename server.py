af0d8f2b6490ab3ed36a36239e67a4375b71a061
ericlbarreto <ericbarreto521@gmail.com>
2024-07-26 18:41:44 -0300

    feat(ack): changed logic of timers

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import os
-from functions import *     
+from functions import *
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
@@ -12,22 +12,25 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 timers = {}
 
-def start_timer(packet, addr):
-    global timers
-    timer = threading.Timer(ACK_TIMEOUT, retransmit_packet, [packet, addr])
-    timer.start()
-    timers[addr] = timer  # Armazena o timer no dicionário
+file_lock = threading.Lock()
 
-def retransmit_packet(packet, addr):
+def start_timer(packet, addr, seq_num):
     global timers
-    print("ACK não recebido, retransmitindo pacote...")
+    timer_key = (addr, seq_num)
+    timer = threading.Timer(ACK_TIMEOUT, retransmit_packet, [packet, addr, seq_num])
+    timer.start()
+    timers[timer_key] = timer  # Armazena o timer no dicionário
+
+def retransmit_packet(packet, addr, seq_num):
+    global timers
+    print(f"ACK não recebido para o pacote {seq_num}, retransmitindo pacote...")
     sock.sendto(packet, addr)
-    start_timer(packet, addr)  # Reinicia o timer
+    start_timer(packet, addr, seq_num)  # Reinicia o timer
 
 def send_file(filename, name, client, addr):
-    global timers
-    with open(filename, 'rb') as f:
-        file_content = f.read()
+    with file_lock:
+        with open(filename, 'rb') as f:
+            file_content = f.read()
 
     total_size = len(file_content)
     total_packets = (total_size // (BUFFER_SIZE - 100)) + 1
@@ -41,27 +44,23 @@ def send_file(filename, name, client, addr):
         seq_num = i + 1
         packet = f"{randomId}|{total_packets}|{name}|{addr[0]}|{addr[1]}|{content}|{checksum}|{seq_num}".encode('utf-8')
         sock.sendto(packet, client)
-        start_timer(packet, client)  # Inicia o timer
+        start_timer(packet, client, seq_num)  # Inicia o timer para o pacote específico
 
+def send_ack(seq_num, client):
+    ack_message = f"ACK|{seq_num}".encode('utf-8')
+    sock.sendto(ack_message, client)
 
-def send_ack(filename, client):
-    with open(filename, 'rb') as f:
-        file_content = f.read()
-    content = file_content.decode('utf-8') 
-
-    contentWHeader = f"ACK|{content}".encode('utf-8')
-    sock.sendto(contentWHeader, client)
-
-
-def send_message(message, name, client, addr, isAck = False):
+def send_message(message, name, client, addr, isAck=False):
     filename = f'message-{name}.txt'
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(message)
-    if (isAck):
-        send_ack(filename, client)
+    with file_lock:
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(message)
+    if isAck:
+        send_ack(message, client)
     else:
         send_file(filename, name, client, addr)
-    os.remove(filename)
+    with file_lock:
+        os.remove(filename)
 
 def handle_client(data, addr):
     try:
@@ -70,7 +69,6 @@ def handle_client(data, addr):
 
         try:
             message_type, *content = data.decode('utf-8').split('|')
-
         except UnicodeDecodeError as e:
             print(f"Erro de decodificação de dados: {e}")
             return
@@ -85,9 +83,7 @@ def handle_client(data, addr):
 
         elif message_type == "BYE":
             username = content[0]
-
             clients.discard(addr)
-
             message = (f'😪 Usuário {addr} com o username {username} saiu no chat.')
             print(message)
             for client in clients:
@@ -96,21 +92,23 @@ def handle_client(data, addr):
 
         elif message_type == "ACK":
             seq_num = int(content[0])
-            if addr in timers:
-                timers[addr].cancel()  # Cancela o timer de retransmissão
+            timer_key = (addr, seq_num)
+            if timer_key in timers:
+                timers[timer_key].cancel()  # Cancela o timer de retransmissão
                 print(f"ACK recebido do cliente {addr} para o pacote {seq_num}")
+                del timers[timer_key]  # Remove o timer do dicionário
 
         elif message_type in messages:
             total_packets, name, packetData, checksum, seq_num = content
             if checksum == calculate_checksum(packetData):
-                print(f"Checksum válido para o pacote {packetData}")
-                messages[message_type] = { "name": name, "packets": [*messages[message_type]["packets"], packetData] }
+                print(f"Checksum válido para o pacote {seq_num}")
+                messages[message_type]["packets"][seq_num] = packetData
                 
                 send_message(seq_num, name, addr, addr, True)
 
-                if (len(messages[message_type]["packets"]) == int(total_packets)):
+                if len(messages[message_type]["packets"]) == int(total_packets):
                     file_content = bytearray()
-                    for i in range(int(total_packets)):
+                    for i in range(1, int(total_packets) + 1):
                         file_content.extend(messages[message_type]["packets"][i].encode("utf-8"))
                     message_text = file_content.decode('utf-8')
 
@@ -120,17 +118,13 @@ def handle_client(data, addr):
         else: 
             total_packets, name, packetData, checksum, seq_num = content
             if checksum == calculate_checksum(packetData):
-                print(f"Checksum válido para o pacote {packetData}")
-                messages[message_type] = {"name": name, "packets": [packetData] }
+                print(f"Checksum válido para o pacote {seq_num}")
+                messages[message_type] = {"name": name, "packets": {seq_num: packetData} }
 
                 send_message(seq_num, name, addr, addr, True)
 
-                if (total_packets == '1'):
-                    file_content = bytearray()
-                    for i in range(int(total_packets)):
-                        file_content.extend(messages[message_type]['packets'][i].encode("utf-8"))
-                    message_text = file_content.decode('utf-8')
-
+                if int(total_packets) == 1:
+                    message_text = packetData
 
                     for client in clients:
                         if client != addr:
